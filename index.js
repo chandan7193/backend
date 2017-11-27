01953542265b31cce80169de90023eea5ced20d5
chandan7193 <chandan.singh7193@gmail.com>
2017-11-27 19:50:13 +0530

    Enabling Google OAuth API

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const app = express();
 
+
+//clientID - 400155049510-d0o1je6hk3pu43ui5ca71apt89nppcil.apps.googleusercontent.com
+//clientSecret - NrUoE6fy-TROiCNF1JqZuQN0
 passport.use(new GoogleStrategy());
 
 app.get('/', (req, res) => {
