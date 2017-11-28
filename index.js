cae152d60d742ce56e2be6c23bbfb611f8ae06d3
chandan7193 <chandan.singh7193@gmail.com>
2017-11-28 11:08:47 +0530

    Securing Api keys

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const app = express();
 
 
-//clientID - 400155049510-d0o1je6hk3pu43ui5ca71apt89nppcil.apps.googleusercontent.com
-//clientSecret - NrUoE6fy-TROiCNF1JqZuQN0
 passport.use(new GoogleStrategy());
 
 app.get('/', (req, res) => {
