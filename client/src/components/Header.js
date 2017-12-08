a0f6dc7d74d705e254735d54ff89a82ac914e395
chandan7193 <chandan.singh7193@gmail.com>
2017-12-08 11:12:00 +0530

    Additional proxy Rules and Current User Api

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 export default class Header extends Component{
   render(){
     return(
-      <div>
+      <div >
         <nav>
           <div className="nav-wrapper">
             <a href="#" className=" left brand-logo">Emaily</a>
