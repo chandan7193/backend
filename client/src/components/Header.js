7dd889a7cb664812279ace1da73fb0641e48a206
chandan7193 <chandan.singh7193@gmail.com>
2017-12-09 13:58:56 +0530

    Test

---
+++ client/src/components/Header.js
@@ -0,0 +1,39 @@
+import React, {Component} from 'react';
+import { connect }from 'react-redux';
+
+
+ class Header extends Component{
+   renderContent(){
+
+     switch(this.props.auth)  {
+       case null:
+        return
+       case false:
+        return <li><a href = "auth/google" >Login with Google</a></li>;
+       default:
+        return <li>Logged out</li>;
+     }
+   }
+
+  render(){
+
+    return(
+      <div >
+        <nav>
+          <div className="nav-wrapper">
+            <a className=" left brand-logo">Emaily</a>
+            <ul  className="right">
+              {this.renderContent()}
+            </ul>
+          </div>
+        </nav>
+      </div>
+    )
+  }
+}
+
+function mapsStateToProps({auth}){
+  return {auth};
+}
+
+export default connect(mapsStateToProps)(Header);
