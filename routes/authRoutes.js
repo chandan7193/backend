874d8f0e1f4b67e972ed5ba138c2b5dfcf8edcb2
chandan7193 <chandan.singh7193@gmail.com>
2017-12-19 17:53:46 +0530

    Error

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -18,6 +18,18 @@ module.exports = app => {
   })
 
   app.get('/api/current_user',(req,res) =>{
+
      res.send(req.user);
+
   })
+
+  app.get('/api/display',(req,res) =>{
+    console.log('bhej diya');
+    res.send({
+  userId: 1,
+  id: 1,
+  title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+  body: "quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto"
+  })
+    })
 };
