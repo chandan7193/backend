c712d8c2d9ebbd998e85b57d25609e1d8f834810
Chandan Singh <chandan.singh7193@gmail.com>
2018-03-28 10:44:52 +0530

    API test

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,35 +1,36 @@
-const passport = require('passport');
+const passport = require("passport");
+const requireLogin = require("../middlewares/requireLogin");
 
 module.exports = app => {
   app.get(
-    '/auth/google',
-    passport.authenticate('google', {
-      scope: ['profile', 'email']
+    "/auth/google",
+    passport.authenticate("google", {
+      scope: ["profile", "email"]
     })
   );
 
-  app.get('/auth/google/callback', passport.authenticate('google'),(req,res) =>{
-    res.redirect('/surveys');
+  app.get(
+    "/auth/google/callback",
+    passport.authenticate("google"),
+    (req, res) => {
+      res.redirect("/surveys");
+    }
+  );
+
+  app.get("/api/logout", (req, res) => {
+    req.logout();
+    res.redirect("/");
   });
 
-  app.get('/api/logout', (req, res) => {
-    req.logout();
-    res.redirect('/');
-  })
-
-  app.get('/api/current_user',(req,res) =>{
-
-     res.send(req.user);
-
-  })
-
-  app.get('/api/display',(req,res) =>{
-    console.log('bhej diya');
+  app.get("/api/display", (req, res) => {
+    console.log("bhej diya");
     res.send({
-  userId: 1,
-  id: 1,
-  title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-  body: "quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto"
-  })
-    })
+      userId: 1,
+      id: 1,
+      title:
+        "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+      body:
+        "quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto"
+    });
+  });
 };
