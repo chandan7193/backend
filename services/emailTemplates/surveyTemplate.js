0088e8e9d362b5e8c86591aaa5a58cccd26ef416
Chandan Singh <chandan.singh7193@gmail.com>
2018-04-04 16:54:22 +0530

    Before restarting Frontend stuff

--- services/emailTemplates/surveyTemplate.js
+++ services/emailTemplates/surveyTemplate.js
@@ -1,16 +1,20 @@
-module.exports = survey =>{
+const keys = require("../../config/keys");
+
+// To access es6 string intepolation => ${keys.redirectDomain}
+
+module.exports = survey => {
   return `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
   <html>
     <body>
       <div style="height:100%;margin:0;padding:0;width:100%;background-color:#fafafa;">
-        
+
         <center>
           <table align="center" border="0" cellpadding="0" cellspacing="0" width="100%" id="m_-1311019695435276457bodyTable" style="height:100%;border-collapse:collapse;margin:0;padding:0;width:100%;background-color:#fafafa;">
             <tbody>
               <tr>
                 <td align="center" valign="top" id="m_-1311019695435276457bodyCell" style="height:100%;margin:0;padding:10px;width:100%;border-top:0;">
-                  
-                  
+
+
                   <table border="0" cellpadding="0" cellspacing="0" width="100%" class="m_-1311019695435276457templateContainer" style="border-collapse:collapse;border:0;max-width:600px !important;">
                     <tbody>
                       <tr>
@@ -26,10 +30,10 @@ module.exports = survey =>{
                                     <tbody>
                                       <tr>
                                         <td class="m_-1311019695435276457mcnImageContent" valign="top" style="padding-right:0px;padding-left:0px;padding-top:0;padding-bottom:0;text-align:center;">
-                                          
+
                                           <a href="https://www.letsdogether.com/experiences/turn-into-a-zombie" title="" target="_blank">
                                             <img alt="" src="https://ci6.googleusercontent.com/proxy/Hew09Cyzzz1M-HfvUy-AS3Sil8Ek2VEmzqQ1eMY_111-xqcdXhRuKsyKLPaELdo2CDA5zF0P3ZTpc07wfR7pGjJnLJO1Pr8QgpLoeoe7KQN_L_Jv3F5qahpP-1kcCehhjknPO9ANnd4JcFX3BimT2g9lkrFHtB7tFgBj8gs=s0-d-e1-ft#https://gallery.mailchimp.com/ef09da77ccaa5d2caf393feba/images/9c765e86-e5cb-4d20-b733-085a05ae0d9a.jpg" width="600" style="max-width:600px;padding-bottom:0;display:inline!important;vertical-align:bottom;border:0;height:auto;outline:none;text-decoration:none" class="m_-1311019695435276457mcnImage CToWUd"></a>
-                                            
+
                                           </td>
                                         </tr>
                                       </tbody>
@@ -47,16 +51,20 @@ module.exports = survey =>{
                                 <tr>
                                   <td align="center" valign="middle" style="font-family:Arial;font-size:12px;padding:15px;">
                                     <div class="m_-1311019695435276457btn" style="background-color:#ee4b3a;border:none;padding:15px;border-radius:25px;width:50%;">
-                                      <a title="Buy Now" style="font-weight:normal;letter-spacing:normal;line-height:100%;text-align:center;text-decoration:none;color:#fff;display:block;" href="https://www.letsdogether.com/experiences/turn-into-a-zombie" target="_blank">Become a zombie</a>
+                                      <a title="Buy Now" style="font-weight:normal;letter-spacing:normal;line-height:100%;text-align:center;text-decoration:none;color:#fff;display:block;" href="${
+                                        keys.redirectDomain
+                                      }/experiences/turn-into-a-zombie" target="_blank">Become a zombie</a>
                                     </div>
                                   </td>
                                   <td align="center" valign="middle" style="font-family:Arial;font-size:12px;padding:15px;">
-                                    
+
                                     <div class="m_-1311019695435276457btn" style="background-color:#ee4b3a;border:none;padding:15px;border-radius:25px;width:50%;">
-                                      <a title="Buy Now" style="font-weight:normal;letter-spacing:normal;line-height:100%;text-align:center;text-decoration:none;color:#fff;display:block;" href="https://www.letsdogether.com/experiences/run-from-zombies" target="_blank">Run from zombie</a>
+                                      <a title="Buy Now" style="font-weight:normal;letter-spacing:normal;line-height:100%;text-align:center;text-decoration:none;color:#fff;display:block;" href="${
+                                        keys.redirectDomain
+                                      }/experiences/run-from-zombies" target="_blank">Run from zombie</a>
                                     </div>
                                   </td>
-                                  
+
                                 </tr>
                               </tbody>
                             </table>
@@ -76,11 +84,11 @@ module.exports = survey =>{
                                                       <tbody>
                                                         <tr>
                                                           <td align="center" valign="top">
-                                                            
-                                                            
-                                                            
-                                                            
-                                                            
+
+
+
+
+
                                                             <table align="left" border="0" cellpadding="0" cellspacing="0" style="display:inline;border-collapse:collapse;">
                                                               <tbody>
                                                                 <tr>
@@ -95,8 +103,8 @@ module.exports = survey =>{
                                                                                   <td align="center" valign="middle" width="24" class="m_-1311019695435276457mcnFollowIconContent">
                                                                                     <a href="http://www.twitter.com/" target="_blank"><img src="https://ci6.googleusercontent.com/proxy/azrYTZX3UwhAIrxrjcjAuH62gbJ3hI-sR7S1gh33ZhYpcdWo3RwdttLzFOpZuNwTIgiL2_4MPlXuugq8Xq3VXd4zIPh9_51U6D1MJEgaTfLI2HHlU6h0d6Pbf5hxME0ysg=s0-d-e1-ft#https://cdn-images.mailchimp.com/icons/social-block-v2/light-twitter-48.png" style="display:block;border:0;height:auto;outline:none;text-decoration:none" height="24" width="24" class="CToWUd" alt="light-twitter-48.png"></a>
                                                                                   </td>
-                                                                                  
-                                                                                  
+
+
                                                                                 </tr>
                                                                               </tbody>
                                                                             </table>
@@ -122,8 +130,8 @@ module.exports = survey =>{
                                                                                   <td align="center" valign="middle" width="24" class="m_-1311019695435276457mcnFollowIconContent">
                                                                                     <a href="http://www.facebook.com" target="_blank"><img src="https://ci4.googleusercontent.com/proxy/FTY_Y2GcQoBc9F9WjcJfZbTXjZ6YKb6f1VzVtrXgp4va6YixuqJM-ldbD1Wcw_0T6xCaIPHkcoY9j6I47pYE-u-7vNGo_vvq70OOhdJmNYrP16uHAdgqPZOYkU1R5mL3ZbQ=s0-d-e1-ft#https://cdn-images.mailchimp.com/icons/social-block-v2/light-facebook-48.png" style="display:block;border:0;height:auto;outline:none;text-decoration:none" height="24" width="24" class="CToWUd" alt="light-facebook-48.png"></a>
                                                                                   </td>
-                                                                                  
-                                                                                  
+
+
                                                                                 </tr>
                                                                               </tbody>
                                                                             </table>
@@ -149,8 +157,8 @@ module.exports = survey =>{
                                                                                   <td align="center" valign="middle" width="24" class="m_-1311019695435276457mcnFollowIconContent">
                                                                                     <a href="http://mailchimp.com" target="_blank"><img src="https://ci3.googleusercontent.com/proxy/0AKzxdi-dUflhfA8a2dkVPr15h5wRN5PhgVNhs8pW6mNYlLE3iohC1SMa3izgphTtE292zvIvA9U_J4CNfK4NOTywn9BXcS8oJnvm6LwPYS_VDetRz0URJUChVOBZA=s0-d-e1-ft#https://cdn-images.mailchimp.com/icons/social-block-v2/light-link-48.png" style="display:block;border:0;height:auto;outline:none;text-decoration:none" height="24" width="24" class="CToWUd" alt="light-link-48.png"></a>
                                                                                   </td>
-                                                                                  
-                                                                                  
+
+
                                                                                 </tr>
                                                                               </tbody>
                                                                             </table>
@@ -186,14 +194,14 @@ module.exports = survey =>{
                               <tbody class="m_-1311019695435276457mcnTextBlockOuter">
                                 <tr>
                                   <td valign="top" class="m_-1311019695435276457mcnTextBlockInner" style="padding-top:9px;">
-                                    
-                                    
-                                    
+
+
+
                                     <table align="left" border="0" cellpadding="0" cellspacing="0" style="max-width:100%;min-width:100%;border-collapse:collapse;" width="100%" class="m_-1311019695435276457mcnTextContentContainer">
                                       <tbody>
                                         <tr>
                                           <td valign="top" class="m_-1311019695435276457mcnTextContent" style="padding-top:0;padding-right:18px;padding-bottom:9px;padding-left:18px;color:#656565;font-family:Helvetica;font-size:12px;line-height:150%;text-align:center;">
-                                            
+
                                             <div style="text-align:center;"><span style="font-size:12px">Made with ♥ at Dogether © 2018. All rights reserved.<br>
                                               Dogether App Pvt Ltd, Parel, Mumbai, Maharashtra 400015</span>
                                             </div>
