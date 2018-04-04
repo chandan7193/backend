e31cb03787f751bf26ed66ad9a1557bacae85b63
Chandan Singh <chandan.singh7193@gmail.com>
2018-04-04 21:04:38 +0530

    Wiring up custom fields

---
+++ client/src/components/surveys/SurveyField.js
@@ -0,0 +1,11 @@
+// SurveyField contains the logic to display single input
+
+import React from "react";
+
+export default ({ input }) => {
+  return (
+    <div>
+      <input {...input} />
+    </div>
+  );
+};
