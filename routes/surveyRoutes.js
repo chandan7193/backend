642db98f46bfbfd2c77abb7046ca81010d930163
Chandan Singh <chandan.singh7193@gmail.com>
2018-04-05 19:19:10 +0530

    redirect on submit

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -23,20 +23,15 @@ module.exports = app => {
 
     const mailer = new Mailer(survey, surveyTemplate(survey));
 
-
     try {
       await mailer.send();
       await survey.save();
 
-      req.user.credits-=1;
       const user = await req.user.save();
 
       res.send(user);
-    } catch(err) {
+    } catch (err) {
       res.status(422).send(err);
     }
   });
-
-
-  
 };
